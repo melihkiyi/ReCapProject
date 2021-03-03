3e32442b96df1895235160eadebdc8bb72c680a0
Melih Kıyı <melihkiyi@outlook.com>
2021-03-03 21:00:26 +0300

    RentACar

--- Business/Constans/Messages.cs
+++ Business/Constans/Messages.cs
@@ -10,6 +10,11 @@ namespace Business.Constans
         public static string CarDescriptionInvalid = "Araba açıklaması geçersiz";
         public static string CarDeleted = "Araba silindi";
         public static string CarUpdate = "Araba bilgileri güncellendi";
-        public static string CarListed="ArabaListelendi";
+        public static string CarListed="Araba Listelendi";
+
+        public static string BrandAdded = "Marka eklendi";
+        public static string BrandDeleted = "Marka silindi";
+        public static string BrandUpdated = "Marka Güncellendi";
+        public static string BrandListed = "Marka Listelendi";
     }
 }
